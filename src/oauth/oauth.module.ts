0949f7a3639433a20487b0f8bd94cad30a7756ab
siwonpada <psw041115@gmail.com>
2023-08-24 23:52:56 +0900

    feat: oauth

--- src/oauth/oauth.module.ts
+++ src/oauth/oauth.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OauthController } from './oauth.controller';
+import { OauthService } from './oauth.service';
 
-@Module({})
+@Module({
+    controllers: [OauthController],
+    providers: [OauthService],
+})
 export class OauthModule {}
